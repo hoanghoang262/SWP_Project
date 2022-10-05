306a1bbc7f86a9e5ae9769f00495875e50fa19bd
hoang262 <hoangcanada2002@gmail.com>
2022-10-05 11:01:44 +0700

    add narbar demo

--- Client/HappyProgramming/src/App.tsx
+++ Client/HappyProgramming/src/App.tsx
@@ -6,18 +6,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element = {<HomePage/>}/> {/* home page */}
-        <Route path='/signIN'>
-          <Route path='/resetPw'/>{/* lay lai mk neu quen  */}
-        </Route> {/* sign in page */}
         <Route path='/signUp'/> {/* sign up page */}
         <Route path="userProfile"/>
         <Route path='/about'/>{/* about page show up general infomatio nabout web */}
         <Route path='contact'/>{/* page to contact with admin */}
         <Route path='/coursePage'/> {/* page show up list of course */}
-        <Route path='/admin'>
-          <Route path='/userControl'/>
-          <Route path='/courseControl'/>
-        </Route>
       </Routes>
     </BrowserRouter>
   )
--- Client/HappyProgramming/src/Page/HomePage.tsx
+++ Client/HappyProgramming/src/Page/HomePage.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Narbar from "../Component/Narbar";
 
-function HomePage(){
-    return(
-        <div className="text-3xl font-bold">home page</div>
+function HomePage() {
+    return (
+        <div>
+            <Narbar />
+            <div className="h-20 w-full"></div>
+            <div className="text-3xl font-bold">home page</div>
+        </div>
     )
-} 
+}
 
 export default HomePage
