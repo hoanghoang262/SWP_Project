5745f1cd1bbc566b805c67c1d19cc2ecac6aefa4
hoang262 <hoangcanada2002@gmail.com>
2022-10-23 20:05:50 +0700

    start branch work

--- Client/HappyProgramming/src/Component/UserProfileForm.jsx
+++ Client/HappyProgramming/src/Component/UserProfileForm.jsx
@@ -1,9 +1,27 @@
 import { Box, Container, Typography, Stack, TextField } from '@mui/material';
+import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { userInfoState } from '../Recoil/Atom';
 
 export default function UserProfileForm() {
+
+  //set state manager
   const [userInfo, setUserInfo] = useRecoilState(userInfoState);
+  const [email,setEmail] = useState("");
+  const [firstName,setFirstName] = useState("");
+  const [lastName,setLastName] = useState("");
+  const [phoneNumber,setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [contry,setContry] = useState("");
+  const [city,setCity] = useState("");
+  const [zipCode,setZipCode] = useState("");
+  const [state,setState] = useState("");
+
+  //even handle
+  const emailHandle = () =>{
+    
+  } 
+
   return (
     <Stack spacing={2} direction="column">
       <Box
@@ -15,22 +33,21 @@ export default function UserProfileForm() {
       </Box>
       <TextField variant="outlined" label="Email" value = {userInfo.email} />
       <TextField variant="outlined" label="Contacts Number" value = {userInfo.phoneNumber}/>
+      <Box
+        display="flex"
+        sx={{ flexDirection: { md: 'row', xs: 'column' }, gap: 3 }}
+      >
+        <TextField variant="outlined" label="Country" fullWidth />
+        <TextField variant="outlined" label="City" fullWidth />
+      </Box>
       <TextField variant="outlined" label="Address" value = {userInfo.address}/>
       <Box
         display="flex"
         sx={{ flexDirection: { md: 'row', xs: 'column' }, gap: 3 }}
       >
-        <TextField variant="outlined" label="City" fullWidth />
+        <TextField variant="outlined" label="Zip code" fullWidth />
         <TextField variant="outlined" label="State" fullWidth />
       </Box>
-      <Box
-        display="flex"
-        sx={{ flexDirection: { md: 'row', xs: 'column' }, gap: 3 }}
-      >
-        <TextField variant="outlined" label="Zip code" fullWidth />
-        <TextField variant="outlined" label="Country" fullWidth />
-      </Box>
-      <TextField variant="outlined" label="Password" type="password" />
     </Stack>
   );
 }
